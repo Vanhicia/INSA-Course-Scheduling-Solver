1484ade2b19f31af793760cdb3ef6d07e3bf2010
KimiNako <katran@etud.insa-toulouse.fr>
2019-03-10 21:50:06 +0100

    add another version of mini project

---
+++ mini_project_v2.py
@@ -0,0 +1,61 @@
+from Numberjack import *
+
+
+def get_model (N):
+    slots = 17
+    number_of_weeks = N
+    limit_hours_course = 5  # leveling factor
+    course_1 = 40  # Maths
+    course_2 = 30  # CS, in cs_rooms
+    course_3 = 10  # Chemistry
+    course_4 = 20  # English
+    course_5 = 10 # ppi
+    lab_1 = 15  # CS labs
+
+    course_list = [course_1, course_2, course_3, course_4, course_5]
+    lab_list = [lab_1]
+
+    planning_course = Matrix(len(course_list), number_of_weeks, 1, limit_hours_course)
+    planning_labs = Matrix(len(lab_list), number_of_weeks, 1, limit_hours_course)
+
+    model = Model(
+
+        # Courses
+        [Sum(row) == hours for (row, hours) in zip(planning_course.row, course_list)],
+        [Sum(col) < slots for col in planning_course.col],
+
+        # Labs
+        [Sum(row) == hours for (row, hours) in zip(planning_labs.row, lab_list)],
+        [Sum(col) < slots//2 for col in planning_labs.col]
+    )
+
+    return planning_course, planning_labs, model
+
+
+def solve(param):
+    planning_course, planning_labs, model = get_model(param['N'])
+    solver = model.load(param['solver'])
+    solver.setVerbosity(param['verbose'])
+    solver.setHeuristic(param['var'], param['val'], param['rand'])
+    solver.setTimeLimit(param['cutoff'])
+
+    if param['restart'] == 'yes':
+        solver.solveAndRestart()
+    else:
+        solver.solve()
+
+    out = ''
+    if solver.is_sat():
+        out = '\nCourses: \n' + str(planning_course)
+        out += '\n\nLabs: \n' + str(planning_labs)
+    out += ('\n\nNodes: ' + str(solver.getNodes()))
+    return out
+
+
+default = {'solver': 'Mistral', 'N': 10, 'var': 'MinDomain',
+           'val': 'RandomMinMax', 'restart': 'yes', 'rand': 2, 'verbose': 0, 'cutoff': 10}
+
+
+if __name__ == '__main__':
+    param = input(default)
+    print(solve(param))
