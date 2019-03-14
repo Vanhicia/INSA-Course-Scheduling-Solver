191caa5d147c3445bd123a01919f58e0d08ec840
PierreBinet <pbinet@etud.insa-toulouse.fr>
2019-03-14 12:37:06 +0100

    cleaned the project and added comments

--- mini_project_v2.py
+++ mini_project_v2.py
@@ -2,11 +2,11 @@ from Numberjack import *
 
 
 def get_model (N):
-    slots = 17
+    slots = 17  # Max number of hours per week
     number_of_weeks = N
     limit_hours_course = 5  # leveling factor
 
-    #  Format : [Total hours, Number of Experiments]
+    # Format : [Lecture hours, Number of Experiments]
     course_1 = [40, 5]  # Maths
     course_2 = [30, 1]  # CS, in cs_rooms
     course_3 = [10, 5]  # Chemistry
@@ -17,12 +17,20 @@ def get_model (N):
     course_list = [course_1, course_2, course_3, course_4, course_5]
     experiment_list = [experiment_1]
 
+    # Groups are represented by a list of the courses they are taking
+    group1 = [course_1, course_2, course_3, course_4, experiment_1]
+    group2 = [course_1, course_2, course_3, course_5, experiment_1]
+    group_list = [group1, group2]
+
     planning_course = Matrix(len(course_list), number_of_weeks, 0, limit_hours_course)
-    planning_experiments = Matrix(len(experiment_list), number_of_weeks, 1, limit_hours_course)
+    planning_experiments = Matrix(len(experiment_list), number_of_weeks, 1, limit_hours_course) #pourquoi le min est 1 ?
 
     model = Model(
 
         # Courses
+        # On the matrix, each row represents a course, and each column represent a week.
+        # We then want the sum of a row to be equal to the corresponding course's total hours
+        # and the sum of a column to be less than the max hours of class per week
         [Sum(row) == hours[0] for (row, hours) in zip(planning_course.row, course_list)],
         [Sum(col) < slots for col in planning_course.col],
 
